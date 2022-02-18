20a8f954c1d572eff4034d57e3d61e1917db8f56
OoChip <oochip2001@gmail.com>
2022-02-17 22:58:47 -0500

    version 2

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-function draw (lado){
-    for (let i = 1 ; i <= lado ; i++){
-        for (let j = 1 ; j <= lado ; j++ ){
-            let area = document.createElement ("div")
-            area.className = "div";
-            area.addEventListener("mouseover", hovering)
-            container.appendChild(area)
-        }
+const container = document.getElementById("grid-container");
+
+function random(number) {
+    return Math.floor(Math.random() * number + 1);
+}
+
+function makeRows(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", function bgChange() {
+            const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+            cell.style.backgroundColor = rndCol;
+        });
+    };
+};
+
+makeRows(16,16);
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", function changeSize(size) {
+    size = prompt("Enter a value between 0 to 101");
+    container.innerHTML = "";
+    if (0 < size && size <= 100) {
+        makeRows(size, size);
+    } else {
+        makeRows(16, 16);
+        alert ("Only type values between 1 to 100");
     }
-}
-
-function hovering (e){
-    e.target.style.backgroundColor = "green";
-}
-
-function reset (e){
-    lado = Number(prompt ("introduzca numero"))
-    draw(lado)
-}
-
-let container = document.querySelector (".container")
-let lado = 16
-draw(lado)
-let btn = document.createElement ("button")
-btn.innerText = "Reset"
-btn.addEventListener("click", reset)
-document.body.insertBefore(btn, container);
+});
