68b92ba5d32b257c957877f389371bab030b1cc7
OoChip <oochip2001@gmail.com>
2022-02-18 14:04:26 -0500

    final version

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-function sacula () {
-    return  Math.floor(Math.random() * 128)
-    console.log(sacula);
-}
+//Generate a random number between 0 and 255 to be used as RGB code.
+function sacula () {return  Math.round(Math.random() * 255)}
 
+//Generates a new RGB color and paints with it the cell that generated the event (mouseover).
 function newColor (e){   
-    const newColor = `rgb(${sacula()},${sacula()},${sacula()})`;
-    let cell= e.target
-    cell.style.backgroundColor = newColor
+    const newColor = `rgb(${sacula()},${sacula()},${sacula()})`
+    e.target.style.backgroundColor = newColor
 }
 
-function makeCell(rows = 16, cols = 16) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    container.innerHTML = "";
+//Generates all the cells and fills the grid with them.
+function buildCells(rows = 16, cols = 16) {
+    container.style.setProperty("--grid-rows", rows)
+    container.style.setProperty("--grid-cols", cols)
+    container.innerHTML = "" //Important. Clean the grid before adding the new cells (source of all evil).
     for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
+        const cell = document.createElement("div")
         cell.addEventListener("mouseover", newColor)
-        container.appendChild(cell).className = "grid-item";
-    };
-};
+        container.appendChild(cell).className = "grid-item"
+    }
+}
 
-function reSize (){
+//Rebuild the grid.
+function rebuild (){
     let size = Number(prompt("Enter a value between 0 to 100"))
     if (0 < size && size <= 100){
-        makeCell(size, size)}
-    else if(size > 100){
-        makeCell()
-        alert ("the value must be between o to 100")}
+        buildCells(size, size)
+    } else {
+        buildCells()
+        alert ("The value must be between o to 100")}
 }
 
 const container = document.querySelector(".grid-container");
-makeCell();
+buildCells()
 
-const button = document.getElementById("button");
-button.addEventListener ("click", reSize)
+const button = document.querySelector(".button");
+button.addEventListener ("click", rebuild)
